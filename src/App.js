4ca4e288997897a3f2dc5109480bcb7073b40874
Cesar Restrepo <cesar.restrepo@turner.com>
2019-08-18 15:04:03 -0500

    Cleanup setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import { NavigationDrawer } from 'react-md';
-import logo from './logo.svg';
+import { Toolbar } from 'react-md';
+// import logo from './logo.svg';
 import './App.css'
 
 function App() {
   return (
-    <NavigationDrawer
-        drawerTitle="react-md with CRA"
-        toolbarTitle="Welcome to react-md"
-      >
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </NavigationDrawer>
+    <Toolbar
+      themed
+      title="Jhon Doe co."
+    />
   );
 }
 
